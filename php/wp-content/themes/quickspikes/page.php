90030b84050cc64578d874f5d5ca4de3cf58390f
Owen Corso <owen@strattonimaging.com>
2010-04-08 19:00:50 -0400

    experimenting with wp e-commerce

--- php/wp-content/themes/quickspikes/page.php
+++ php/wp-content/themes/quickspikes/page.php
@@ -1,4 +1,32 @@
 <?php get_header(); ?>
 <!-- this is where page stuff goes -->
-<h1>Hi this is a new page</h1>
+<div id="body_container">
+	  
+	  <?php if(have_posts()): ?>
+	  <?php while(have_posts()):the_post(); ?>
+	  <div class="post">
+
+	    <div class="entry">
+	      <?php the_content("[Read more &rarr;]"); ?>
+	      <p class="postmetadata">
+	        <?php the_tags( 'Tags: ', ', '); ?>//
+	           Category <?php the_category(', ') ?>
+	        <?php edit_post_link('Edit', ' &#124; ', ''); ?>
+	        // <strong>
+	        <?php comments_popup_link('Add Comment &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+	        </strong> </p>
+	    </div>
+	  </div>
+	  <?php endwhile; ?>
+		  <?php else: ?>
+	  <div class="post" id="post-<?php the_ID(); ?>">
+	    <h2>
+	      <?php _e('Not Found'); ?>
+	    </h2>
+	  </div>
+	  <?php endif; ?>
+	 
+</div>
+
+
 <?php get_footer(); ?>
