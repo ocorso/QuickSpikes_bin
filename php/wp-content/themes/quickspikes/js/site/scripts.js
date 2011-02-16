666c9f5dd6142c49c5cd148330e7b7ac3ddac60b
Owen Corso <owen@ored.net>
2011-02-16 10:02:44 -0500

    cookie set for logo expand on initial expand

--- php/wp-content/themes/quickspikes/js/site/scripts.js
+++ php/wp-content/themes/quickspikes/js/site/scripts.js
@@ -2,6 +2,10 @@
 
 */
 
+	//**********************************************************
+	//						Doc Ready 
+	//**********************************************************
+	
  jQuery(document).ready(function($){
  	log("dom is ready");
  	
@@ -9,17 +13,47 @@
  	
  	
  	addHandlers();
- 	
- 	
+ 	setupHomepageCarousel();
+	cookieMonster();
+	
+	//**********************************************************
+	//						Workers
+	//**********************************************************
+	function setupHomepageCarousel(){
+
+	 	$('#slider').nivoSlider({
+			effect:'fade', //Specify sets like: 'random,fold,fade,sliceDown'
+			slices:15,
+			animSpeed:500, //Slide transition speed
+			pauseTime:6000,
+			startSlide:0, //Set starting Slide (0 index)
+			directionNav:false, //Next & Prev
+			directionNavHide:true, //Only show on hover
+			controlNav:false, //1,2,3...
+			controlNavThumbs:false, //Use thumbnails for Control Nav
+	      	controlNavThumbsFromRel:false, //Use image rel for thumbs
+			controlNavThumbsSearch: '.jpg', //Replace this with...
+			controlNavThumbsReplace: '_thumb.jpg', //...this in thumb Image src
+			keyboardNav:true, //Use left & right arrows
+			pauseOnHover:false, //Stop animation while hovering
+			manualAdvance:false, //Force manual transitions
+			captionOpacity:0.0, //Universal caption opacity
+			beforeChange: function(){},
+			afterChange: function(){},
+			slideshowEnd: function(){} //Triggers after all slides have been shown
+		});
+
+	}
+	
 	function addHandlers(){
 	
 		$("nav ul li").hover(
   			function () {//over
-  				log("we're over");
+//log("we're over");
   				$(this).removeClass("nav-link").addClass("nav-hover");
   				$(this).find('.nav-a').css("color", "#ff0");
   			},
-  			function () {//out
+  			function () {//main nav out handler
   				$(this).removeClass("nav-hover").addClass("nav-link");
   				$(this).find('.nav-a').css("color", "#fff");
   			}
@@ -28,40 +62,44 @@
 			log("clicked on: "+ url);
 			document.location.replace(url);
 		});//end nav li event handler
-		$("#logo").hover(
-			function (){//over
-				log('logo over');
-				$("#logo_reveal").animate({width:"318px"},400);
-			},
-			function (){//out
-				log('logo out');
-				$("#logo_reveal").animate({width:"0px"},400);
-			}
-		);
-
+		
+		//logo handlers
+		$("#logo").hover(	expandLogo,
+							collapseLogo);
 	}//end function
 
+	//**********************************************************
+	//						Handlers
+	//**********************************************************
+	
+	function expandLogo($e){
+		$("#logo_reveal").animate({width:"318px"},400);
+	}
+	function collapseLogo($e){//out
+		$("#logo_reveal").animate({width:"0px"},400);
+	}
+	
+		
+	//**********************************************************
+	//						Cookie
+	//**********************************************************
 
-	$('#slider').nivoSlider({
-		effect:'fade', //Specify sets like: 'random,fold,fade,sliceDown'
-		slices:15,
-		animSpeed:500, //Slide transition speed
-		pauseTime:6000,
-		startSlide:0, //Set starting Slide (0 index)
-		directionNav:false, //Next & Prev
-		directionNavHide:true, //Only show on hover
-		controlNav:false, //1,2,3...
-		controlNavThumbs:false, //Use thumbnails for Control Nav
-      	controlNavThumbsFromRel:false, //Use image rel for thumbs
-		controlNavThumbsSearch: '.jpg', //Replace this with...
-		controlNavThumbsReplace: '_thumb.jpg', //...this in thumb Image src
-		keyboardNav:true, //Use left & right arrows
-		pauseOnHover:false, //Stop animation while hovering
-		manualAdvance:false, //Force manual transitions
-		captionOpacity:0.0, //Universal caption opacity
-		beforeChange: function(){},
-		afterChange: function(){},
-		slideshowEnd: function(){} //Triggers after all slides have been shown
-	});
+	function cookieMonster(){
+			//document.cookie =   'quickspikes=yet another test; expires=Mon, 14 Feb 2011 13:50:11 UTC; path=/';
+		log(document.cookie);
+		log(document.cookie.indexOf("quickspikes"));
+
+		if (document.cookie.indexOf("quickspikes") == -1){
+			log("quickspikes cookie NOT present");
+			document.cookie = "quickspikes=1";
+			log(document.cookie);
+			expandLogo();
+			setTimeout(collapseLogo, 3000);
+
+		}//end if we don't have a quickspikes cookie
+	
+	}//end function cookieMonster
+	
+	
  });//end doc ready
 
