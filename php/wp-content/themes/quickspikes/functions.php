7c668c3adeab25682cd8853c30944710e4e97e67
Owen Corso <owen@ored.net>
2011-03-07 00:15:14 -0500

    umm wordpress updated

--- php/wp-content/themes/quickspikes/functions.php
+++ php/wp-content/themes/quickspikes/functions.php
@@ -7,6 +7,22 @@ add_shortcode('form-shortcode', 'form_shortcode_handler');
 add_shortcode('image','image_shortcode');
     
 // =================================================
+// ================ @Remove
+// =================================================
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'feed_links_extra');
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'parent_post_rel_link');
+remove_action('wp_head', 'start_post_rel_link');
+remove_action('wp_head', 'adjacent_posts_rel_link');
+remove_action('wp_head', 'locale_stylesheet');
+remove_action('wp_head', 'noindex');
+remove_action('wp_head', 'wp_print_styles');
+remove_action('wp_head', 'wp_print_head_scripts');
+
+// =================================================
 // ================ @Enqueue
 // =================================================
 function my_init_method() {
