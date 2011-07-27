c18f0b7a8631939ef69bd07daded56e3c93bd44f
Owen Corso <owen@ored.net>
2011-07-26 22:39:49 -0400

    gold cart update

--- php/wp-config.php
+++ php/wp-config.php
@@ -17,10 +17,10 @@
 // ** MySQL settings - You can get this info from your web host ** //
 
 //dev on local
-define('DB_NAME', 'quickspikesdb');
-define('DB_USER', 'quickspikesdb');
-define('DB_PASSWORD', 'Vamp5near');
-define('DB_HOST', 'localhost');
+define('DB_NAME', 'qsdb');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost:8888');
 
 //production
 /*
